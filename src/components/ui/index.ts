f0e0232c85b47275e67a0a31889518c30b22faea
kanta1207 <kan0712livepool@gmail.com>
2024-02-14 10:55:04 -0800

    feat: index export components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,24 @@
 export { Button, buttonVariants } from './Button';
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from './Dialog';
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './Card';
+export { Avatar, AvatarImage, AvatarFallback } from './Avatar';
+
 export { Icon } from './Icon';
